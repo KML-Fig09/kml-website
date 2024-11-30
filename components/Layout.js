8cbd10a261544b84b3caec11c3a4747d8ce0cc73
KML-Fig09 <kim.littler1@gmail.com>
2024-11-30 18:09:05 +0000

    **Altered to fix compatibility**

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import Head from 'next/head';
-import Header from 'components/Header'
+import Header from '\components\Header.js';
 
 export default function Layout({ children }) {
   return (
     <>
       <Head>
-        <title>Kim Littler Portfolio</title>
+        <title>Kim Littler</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <main className="bg-gray-100 text-gray-800 min-h-screen">{children}</main>
