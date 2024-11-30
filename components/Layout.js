453dd8b9b94530dad00096d48098a985753ab425
KML-Fig09 <kim.littler1@gmail.com>
2024-11-30 18:27:32 +0000

    Fixed issues

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import Header from '\components\Header.js';
+import Header from './Header';
 
 export default function Layout({ children }) {
   return (
     <>
-      <Head>
+      <Header>
         <title>Kim Littler</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+      </Header>
       <main className="bg-gray-100 text-gray-800 min-h-screen">{children}</main>
     </>
   );
