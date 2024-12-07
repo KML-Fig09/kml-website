e35f285104d86717170bec3a272a23d39874d17f
KML-Fig09 <kim.littler1@gmail.com>
2024-12-07 17:43:52 +0000

    typo fix

--- components/Header.js
+++ components/Header.js
@@ -23,7 +23,7 @@ export default function Header() {
         <div
           className={`${
             menuOpen ? 'block' : 'hidden'
-          } absolute top-16 left-0 w-full bg-background-dark md:static md:block md:w-auto`}
+          } absolute top-16 right-0 w-full colors.backgroundDark md:static md:block md:w-auto`}
         >
           <ul className="flex flex-col md:flex-row md:space-x-8 text-lg font-medium tracking-wide">
             <li>
