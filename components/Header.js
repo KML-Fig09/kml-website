48e4b6f7d338633c19f340a9d2281a8b85950c07
KML-Fig09 <kim.littler1@gmail.com>
2024-12-12 17:50:49 +0000

    rollback

--- components/Header.js
+++ components/Header.js
@@ -23,9 +23,9 @@ export default function Header() {
         <div
           className={`${
             menuOpen ? 'block' : 'hidden'
-          } absolute top-16 right-0 w-64 bg-background-dark text-foreground-light rounded-md shadow-lg md:static md:block md:w-auto`}
+          } absolute top-16 right-0 w-64 bg-background-dark text-foreground-light rounded-md hover: shadow-lg opacity-100 md:static md:block md:w-auto`}
         >
-          <ul className="flex flex-col md:flex-row md:space-x-8 text-lg font-medium tracking-wide">
+          <ul className="flex flex-col bg-background-dark md:flex-row md:space-x-8 text-lg font-medium tracking-wide">
             <li>
               <Link
                 href="/technical"
